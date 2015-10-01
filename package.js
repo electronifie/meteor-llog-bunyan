ee30edfb4642c61347c8b88ffc355088c92b3671
Matt Walters <mattwalters5@gmail.com>
2015-10-01 16:24:11 -0400

    adding name. nuking test stuff. updating to 0.0.10 of llog

--- package.js
+++ package.js
@@ -1,20 +1,16 @@
 Package.describe({
+  name: 'electronifie:meteor-llog-bunyan',
   summary: "llog logging with bunyan support",
-  version: "0.0.0",
+  version: "0.0.1",
   git: "https://github.com/electronifie/meteor-llog-bunyan.git"
 });
 
 Npm.depends({
-  "llog": "0.0.3",
-  "bunyan": "1.2.3"
+  "llog": "0.0.10",
+  "bunyan": "1.5.1"
 });
 
 Package.onUse(function(api) {
   api.versionsFrom('METEOR@0.9.0');
   api.addFiles('electronifie:log-bunyan.js', 'server');
 });
-
-Package.onTest(function(api) {
-  api.use('electronifie:servicebus');
-});
-
